b668c62ec514e2feff900df255b51e635f2bbda5
lekhoa248 <lekhoa248@gmail.com>
2024-12-09 18:01:48 +0700

    no message

--- app.py
+++ app.py
@@ -148,10 +148,17 @@ def delete_ad_request(id):
 
 
 
+# if __name__ == '__main__':
+#     HOST = environ.get('SERVER_HOST', 'localhost')
+#     try:
+#         PORT = int(environ.get('SERVER_PORT', '8000'))
+#     except ValueError:
+#         PORT = 8000
+#     app.run(HOST, PORT, debug=settings.FLASK_DEBUG)
+
+
+def main():
+    app.run(host='0.0.0.0', debug=True)
+
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(environ.get('SERVER_PORT', '8000'))
-    except ValueError:
-        PORT = 8000
-    app.run(HOST, PORT, debug=settings.FLASK_DEBUG)
+    main()
